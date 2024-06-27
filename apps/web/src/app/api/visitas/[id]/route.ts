9a1e5a6c29bce30a9c43fc96edaf985ea9258263
Sergipe <71034184+sergipe085@users.noreply.github.com>
2024-06-27 20:36:59 -0300

    cuida

--- apps/web/src/app/api/visitas/[id]/route.ts
+++ apps/web/src/app/api/visitas/[id]/route.ts
@@ -2,8 +2,8 @@ import { auth } from "@/data/auth";
 import { getVisitaById } from "@/data/visitas";
 import { NextRequest } from "next/server";
 
-export async function GET(req: NextRequest, { param }: any) {
-    const visita = await getVisitaById(param.id);
+export async function GET(req: NextRequest, { params }: any) {
+    const visita = await getVisitaById(params.id);
 
     return new Response(JSON.stringify({
         visita
